22b37a0d6e37ddf6f356cdc831a57c4d13d307fc
TheSolamipe <akinolasolamipe@gmail.com>
2020-08-28 18:15:54 +0100

    Initial commit

---
+++ src/Components/Videos.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./../styles/Videos.css";
+
+function Videos() {
+  return (
+    <div className="video">
+      <h2>This is the video session</h2>
+      {/* {video footer} */}
+      {/* {Video sidebar} */}
+    </div>
+  );
+}
+
+export default Videos;
